ed3debcc42b3f508946536c0f4eaaeddfe6012fd
Hanna <pancakebatter12345@gmail.com>
2019-12-04 17:26:22 -0600

    Click and drag camera

---
+++ Assets/Scripts/MoveByClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByClick : MonoBehaviour
+{
+	public float moveSpeed;
+	float horiz, vert;
+	public Transform container;
+
+    void LateUpdate () {
+		if (Input.GetMouseButton(0)) {
+			horiz = Input.GetAxis("Mouse X");
+			vert = Input.GetAxis("Mouse Y");
+
+			container.Rotate(new Vector3(0, horiz*(-1), 0f)*Time.deltaTime*moveSpeed);
+			transform.Rotate(new Vector3(vert, 0, 0)*Time.deltaTime*moveSpeed);
+		}
+	}
+}
