80456089cd79644226a8ca7fcd1554095e589630
Hanna <pancakebatter12345@gmail.com>
2019-12-05 14:07:51 -0600

    Click to jump works!

---
+++ Assets/Scripts/ClickJump.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickJump : MonoBehaviour
+{
+	public Transform end, cameraContainer;
+
+	void OnMouseUp() {
+		Jump();
+	}
+
+	public void Jump () {
+		cameraContainer.position = end.position;
+	}
+}
--- Assets/Scripts/MoveByClick.cs
+++ Assets/Scripts/MoveByClick.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveByClick : MonoBehaviour
 {
 	public float moveSpeed, mult, flingSpeed;
-	float horiz, vert;
+	float horiz = 0f, vert = 0f, x, y;
 	Quaternion camUp, contUp, camEnd, contEnd;
 	public Transform container;
 
@@ -14,18 +14,8 @@ public class MoveByClick : MonoBehaviour
 			horiz = Input.GetAxis("Mouse X");
 			vert = Input.GetAxis("Mouse Y");
 
-			container.Rotate(new Vector3(0, horiz*(-1), 0f)*Time.deltaTime*moveSpeed);
-			transform.Rotate(new Vector3(vert, 0, 0)*Time.deltaTime*moveSpeed);
-		} else if (Input.GetMouseButtonUp(0)) {
-			horiz = Input.GetAxis("Mouse X");
-			vert = Input.GetAxis("Mouse Y");
-			camUp = transform.rotation;
-			contUp = container.rotation;
-			camEnd = camUp*Quaternion.Euler(0, horiz*(-1)*mult, 0f);
-			contEnd = contUp*Quaternion.Euler(vert*mult, 0, 0);
-		} else {
-			transform.rotation = Quaternion.Lerp(transform.rotation, camEnd, Time.deltaTime*flingSpeed);
-			container.rotation = Quaternion.Lerp(transform.rotation, contEnd, Time.deltaTime*flingSpeed);
+			container.Rotate(new Vector3(0f, horiz*(-1), 0f)*Time.deltaTime*moveSpeed);
+			transform.Rotate(new Vector3(vert, 0f, 0f)*Time.deltaTime*moveSpeed);
 		}
 	}
 }
