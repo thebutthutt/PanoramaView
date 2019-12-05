f2d92fd135741af968cc1b905d1421c417dad0c5
Hanna <pancakebatter12345@gmail.com>
2019-12-04 18:02:21 -0600

    NOT WORKING

--- Assets/Scripts/MoveByClick.cs
+++ Assets/Scripts/MoveByClick.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class MoveByClick : MonoBehaviour
 {
-	public float moveSpeed;
+	public float moveSpeed, mult, flingSpeed;
 	float horiz, vert;
+	Quaternion camUp, contUp, camEnd, contEnd;
 	public Transform container;
 
     void LateUpdate () {
@@ -15,6 +16,16 @@ public class MoveByClick : MonoBehaviour
 
 			container.Rotate(new Vector3(0, horiz*(-1), 0f)*Time.deltaTime*moveSpeed);
 			transform.Rotate(new Vector3(vert, 0, 0)*Time.deltaTime*moveSpeed);
+		} else if (Input.GetMouseButtonUp(0)) {
+			horiz = Input.GetAxis("Mouse X");
+			vert = Input.GetAxis("Mouse Y");
+			camUp = transform.rotation;
+			contUp = container.rotation;
+			camEnd = camUp*Quaternion.Euler(0, horiz*(-1)*mult, 0f);
+			contEnd = contUp*Quaternion.Euler(vert*mult, 0, 0);
+		} else {
+			transform.rotation = Quaternion.Lerp(transform.rotation, camEnd, Time.deltaTime*flingSpeed);
+			container.rotation = Quaternion.Lerp(transform.rotation, contEnd, Time.deltaTime*flingSpeed);
 		}
 	}
 }
