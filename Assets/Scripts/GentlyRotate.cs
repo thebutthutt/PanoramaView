847d6c67f058692a89e0c9b60e755b4e3081ccbf
Hanna <pancakebatter12345@gmail.com>
2019-12-09 17:51:46 -0600

    Looks prettier now

---
+++ Assets/Scripts/GentlyRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GentlyRotate : MonoBehaviour
+{
+    public float rpm;
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 6.0f, 0f)*Time.deltaTime*rpm);
+    }
+}
